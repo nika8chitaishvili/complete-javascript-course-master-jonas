0d5d6a786e5f0f81848ecef087e1798368a2faad
Nika Chitaishvili <nika.chitaishvili@gmail.com>
2023-02-06 02:35:31 +0400

    Add new exercises

--- 01-Fundamentals-Part-1/01-Fundamentals-Part-1.js
+++ 01-Fundamentals-Part-1/01-Fundamentals-Part-1.js
@@ -7,7 +7,7 @@ assign their values according to your own country (population in millions)
 
 const country = "Georgia";
 const continent = "Europe";
-let population = 4000000;
+let population = 4;
 
 console.log("OUTPUT:");
 console.log("Country: " + country);
@@ -77,24 +77,58 @@ console.log("OUTPUT:");
 console.log(population / 2);
 population ++;
 console.log(population);
-console.log(population >= 6000000);
-const description = country + " is in " + continent + ", and it's " + population + " people speak " + language;
+console.log(population >= 6);
+let description = country + " is in " + continent + ", and it's " + population + " million people speak " + language;
 
 console.log(description);
 console.log("--------");
 
 
+console.log("LECTURE: Strings and Template Literals");
+/*
+1. Recreate the 'description' variable from the last assignment, this time using the template literal syntax
+*/
+console.log("OUTPUT:");
+description = `${country} is in  ${continent}, and it's ${population} people speak ${language}`;
+
+console.log(description);
+console.log("--------");
 
 
+console.log("LECTURE: Taking Decisions: if / else Statements");
+/*
+1. If your country's population is greater that 33 million, log a string like this to the
+console: 'Portugal's population is above average'. Otherwise, log a string like
+'Portugal's population is 22 million below average' (the 22 is the average of 33
+minus the country's population)
+2. After checking the result, change the population temporarily to 13 and then to
+130. See the different results, and set the population back to original
+*/
+
+console.log("OUTPUT:");
+population = 4;
+const averageGap = 33 - population;
+
+if (population > 33) {
+  console.log(`${country}'s population is above average`)
+} else{
+  console.log(`${country}'s population is below average by ${averageGap} million`)
+}
+console.log("--------");
 
 
+console.log("LECTURE: Type Conversion and Coercion");
+/*
+1. Predict the result of these 5 operations without executing them:
+'9' - '5';
+'19' - '13' + '17';
+'19' - '13' + 17;
+'123' < 57;
+5 + 6 + '4' + 9 - 4 - 2;
+2. Execute the operations to check if you were right
+*/
 
+console.log("OUTPUT:");
 
-
-
-
-
-
-
-
+console.log("--------");
 
