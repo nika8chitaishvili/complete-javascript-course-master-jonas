327544ddfe4303ffbfe16d77116cb43de0cdd616
Nika Chitaishvili <nika.chitaishvili@gmail.com>
2023-02-09 22:17:45 +0400

    add challenge 2

--- coding-challenges/codingChallenges.js
+++ coding-challenges/codingChallenges.js
@@ -45,10 +45,23 @@ console.log("--------");
 
 console.log("Coding Challenge #2");
 /*
-
+Use the BMI example from Challenge #1, and the code you already wrote, and improve it.
+Your tasks:
+1. Print a nice output to the console, saying who has the higher BMI. The message is either "Mark's BMI is higher than John's!" or "John's BMI is higher than Mark's!"
+2. Use a template literal to include the BMI values in the outputs. Example: "Mark's BMI (28.3) is higher than John's (23.9)!"
+Hint: Use an if/else statement
 */
 
 
 console.log("OUTPUT:");
 
+console.log(`DATA1: Mark's BMI : ${bmiMark1}`);
+console.log(`DATA1: Jogn's BMI : ${bmiJohn1}`);
+if (bmiMark1 > bmiJohn1){
+  console.log(`DATA 1: Mark has a higher BMI than John`);
+}else if (bmiMark1 == bmiJohn1){
+  console.log(`DATA 1: Mark has same BMI as John`);
+}else{
+  console.log(`DATA 1: John's has a higher BMI than Mark`);
+}
 console.log("--------");
