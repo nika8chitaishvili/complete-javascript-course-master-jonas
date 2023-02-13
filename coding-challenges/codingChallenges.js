63c371cefb55c78af31c9c1b759907e6653c56bd
Nika Chitaishvili <nika.chitaishvili@gmail.com>
2023-02-13 23:50:41 +0400

    add challenge 3

--- coding-challenges/codingChallenges.js
+++ coding-challenges/codingChallenges.js
@@ -65,3 +65,51 @@ if (bmiMark1 > bmiJohn1){
   console.log(`DATA 1: John's has a higher BMI than Mark`);
 }
 console.log("--------");
+
+
+
+console.log("Coding Challenge #3");
+/*
+There are two gymnastics teams, Dolphins and Koalas. They compete against each other 3 times. The winner with the highest average score wins a trophy!
+Your tasks:
+1. Calculate the average score for each team, using the test data below 2. Compare the team's average scores to determine the winner of the competition,
+and print it to the console. Don't forget that there can be a draw, so test for that as well (draw means they have the same average score)
+3. Bonus 1: Include a requirement for a minimum score of 100. With this rule, a team only wins if it has a higher score than the other team, and the same time a score of at least 100 points. Hint: Use a logical operator to test for minimum score, as well as multiple else-if blocks 😉
+4. Bonus 2: Minimum score also applies to a draw! So a draw only happens when both teams have the same score and both have a score greater or equal 100 points. Otherwise, no team wins the trophy Test data:
+§ Data 1: Dolphins score 96, 108 and 89. Koalas score 88, 91 and 110
+§ Data Bonus 1: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 123
+§ Data Bonus 2: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106
+*/
+
+console.log("OUTPUT:");
+const dolphins = (96 + 108 + 89)/3;
+const koalas = (88 +91 +110)/3;
+
+const dolphinsBonus1 = (97 + 112 +101)/3;
+const koalasBonus1 = (109 + 95 +123)/3;
+
+const dolphinsBonus2 = (97 + 112 +101)/3;
+const koalasBonus2 = (109 + 95 +106)/3;
+
+console.log(`BONUS 1 - AVERAGES:  
+Dolphins ${dolphinsBonus1}
+Koalas ${koalasBonus1}`);
+
+
+if (dolphinsBonus1 > koalasBonus1 && dolphinsBonus1 >= 100) {
+  console.log(`BONUS 1 RESULT:  Winner - Dolphins with average ${dolphinsBonus1}`)
+}else if(dolphinsBonus1 === koalasBonus1 && dolphinsBonus1 >= 100) {
+  console.log(`BONUS  1 RESULT: Draw`)
+}else if (dolphinsBonus1 < koalasBonus1 && koalasBonus1 >=100){
+  console.log(`BONUS  1 RESULT:  Winner - Koalas with average ${koalasBonus1}`)
+}else{
+  console.log(`BONUS  1 - No Winners`)
+}
+
+console.log("--------");
+
+
+
+console.log(`Coding Challenge #4`);
+
+console.log("--------");
