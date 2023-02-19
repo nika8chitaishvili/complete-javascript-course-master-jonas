3f4e406d0dec757f28d07bf433b7f3b080af455a
Nika Chitaishvili <nika.chitaishvili@gmail.com>
2023-02-19 15:42:15 +0400

    Add new assignment

--- assignments-js-fundamentals/01-Fundamentals-Part-1.js
+++ assignments-js-fundamentals/01-Fundamentals-Part-1.js
@@ -240,3 +240,21 @@ switch (selectedLanguage) {
 console.log("--------");
 console.log("--------");
 
+
+console.log("LECTURE: The Conditional (Ternary) Operator - Part 1");
+/*
+If your country's population is greater than 33 million, use the ternary operator to log a string like this to the console: 'Portugal's population is above average'.
+Otherwise, simply log 'Portugal's population is below average'. Notice how only one word changes between these two sentences!
+2. After checking the result, change the population temporarily to 13 and then to 130. See the different results, and set the population back to original
+*/
+
+console.log("OUTPUT:");
+
+population = 3;
+console.log(
+  `${country}'s population is ${population > 33 ? 'above' :
+  'below'} average`
+  );
+
+console.log("--------");
+console.log("--------");
