0ef4f5aa96239bce026930446466d9995c1e4ec4
Nika Chitaishvili <nika.chitaishvili@gmail.com>
2023-03-06 21:49:32 +0400

    add new exercise

--- assignments-js-fundamentals/02-Fundamentals-Part-2.js
+++ assignments-js-fundamentals/02-Fundamentals-Part-2.js
@@ -107,3 +107,35 @@ console.log(percentages);
 
 console.log("--------");
 console.log("--------");
+
+
+console.log("LECTURE: Basic Array Operations (Methods) - Part 2");
+/*
+1. Create an array containing all the neighbouring countries of a country of your choice. Choose a country which has at least 2 or 3 neighbours. Store the array into a variable called 'neighbours'
+2. At some point, a new country called 'Utopia' is created in the neighbourhood of your selected country. So add it to the end of the 'neighbours' array
+3. Unfortunately, after some time, the new country is dissolved. So remove it from the end of the array
+4. If the 'neighbours' array does not include the country ‘Germany’, log to the console: 'Probably not a central European country :D'
+5. Change the name of one of your neighbouring countries. To do that, find the index of the country in the 'neighbours' array, and then use that index to change the array at that index position. For example, you can search for 'Sweden' in the array, and then replace it with 'Republic of Sweden'.
+*/
+
+const neoghboursGeorgia = ["Armenia", "Azerbaijan", "Turkey","Russia"];
+console.log(neoghboursGeorgia);
+neoghboursGeorgia.push("Utopia");
+console.log(neoghboursGeorgia);
+neoghboursGeorgia.pop();
+console.log(neoghboursGeorgia);
+if(!neoghboursGeorgia.includes('Germany')){
+  console.log("Probably not a central European country :D'")
+}
+
+// Replace value via item's index
+neoghboursGeorgia.splice(3, 3, 'Switzerland');
+console.log(neoghboursGeorgia);
+
+// Replace value  via item's name
+neoghboursGeorgia[neoghboursGeorgia.indexOf("Turkey")] = "Norway";
+console.log(neoghboursGeorgia);
+
+
+console.log("--------");
+console.log("--------");
