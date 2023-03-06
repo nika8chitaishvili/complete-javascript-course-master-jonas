4a0fedffaa84b3b6d9698169266efd3e25de0121
Nika Chitaishvili <nika.chitaishvili@gmail.com>
2023-03-06 22:09:51 +0400

    add - LECTURE: Introduction to Objects

--- assignments-js-fundamentals/02-Fundamentals-Part-2.js
+++ assignments-js-fundamentals/02-Fundamentals-Part-2.js
@@ -150,7 +150,7 @@ const myCountry = {
   country: "Georgia",
   capital: "Tbilisi",
   language: "Georgian",
-  population: 4000000,
+  population: 4,
   neighbours: ["Armenia", "Azerbaijan", "Turkey","Russia"]
 }
 
@@ -158,3 +158,18 @@ console.log(myCountry);
 
 console.log("--------");
 console.log("--------");
+
+console.log("LECTURE: Introduction to Objects - Part 2");
+/*
+1. Using the object from the previous assignment, log a string like this to the console: 'Finland has 6 million finnish-speaking people, 3 neighbouring countries and a capital called Helsinki.'
+2. Increase the country's population by two million using dot notation, and then decrease it by two million using brackets notation.
+*/
+
+console.log(`${myCountry.country} has ${myCountry.population} million ${myCountry.language}-speaking people, ${myCountry.neighbours.length} neighbouring countries and a capital called ${myCountry.capital}.`);
+
+
+console.log("--------");
+console.log("--------");
+
+
+
