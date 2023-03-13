61d10a227b995d1879938235ab82538a3242f3e3
Nika Chitaishvili <nika.chitaishvili@gmail.com>
2023-03-14 00:43:50 +0400

    Add challenge 4

--- coding-challenges/codingChallenges-part2.js
+++ coding-challenges/codingChallenges-part2.js
@@ -122,7 +122,54 @@ if (mark.bmi > john.bmi){
   console.log(`${john.name}'s BMI ${john.bmi}  is higher than ${mark.name}'s BMI ${mark.bmi}`);
 }
 
+
 console.log("--------");
+
+
+
+console.log("Coding Challenge #4- Part 2");
+/*
+1. Create an array 'bills' containing all 10 test bill values
+2. Create empty arrays for the tips and the totals ('tips' and 'totals')
+3. Use the 'calcTip' function we wrote before (no need to repeat) to calculate tips and total values (bill + tip) for every bill value in the bills array. Use a for loop to perform the 10 calculations!
+Test data: 22, 295, 176, 440, 37, 105, 10, 1100, 86 and 52
+Hints: Call ‘calcTip ‘in the loop and use the push method to add values to the tips and totals arrays
+
+*/
+
+console.log("OUTPUT:");
+
+const bills4= [125, 555, 44];
+const tips4= [];
+const totals4= [];
+
+const calcTip4 = function(bill) {
+  return bill>=50 && bill<=300 ? bill*0.15 : bill*0.2;
+}
+
+for (let bill = 0; bill<bills4.length; bill++){
+  const tip = calcTip4(bills4[bill]);
+  tips4.push(tip);
+  totals4.push(tip + bills4[bill]);
+}
+
+
+const calcAverage4 = function (arr) {
+  let sum = 0;
+  for (let i = 0;  i < arr.length; i++){
+    sum+=arr[i];
+  }
+  return sum/arr.length;
+}
+console.log("-ARRAYS-");
+console.log(`Bills4: ${bills4}`);
+console.log(`Tips4: ${tips4}`);
+console.log(`Totals4: ${totals4}`);
+
+console.log("-AVERAGES-");
+console.log(`Bills4 Average: ${calcAverage4(bills4)}`);
+console.log(`Tips4 Average: ${calcAverage4(tips4)}`);
+console.log(`Totals Average: ${calcAverage4(totals4)}`);
 
 
 
